6ba633480040a82e19fe135c6b54ec4f9a52aef5
taikarir <69635722+taikarir@users.noreply.github.com>
2020-10-15 13:32:48 -0400

    Added contact info

--- src/minesweeper.py
+++ src/minesweeper.py
@@ -1,3 +1,9 @@
+#
+#  Author: Tai Karir
+#  First commit: 09/29/2020
+#  Latest update: 15/10/2020
+#  Email: tai.karir@gmail.com
+#
 import random
 #dimensions of the game board
 dimx=9
@@ -94,25 +100,28 @@ def setUpMines(mines,known):
                 break
     known=uncoverZeroes(known)
     return mines,known
-#
+#what happens when you choose a tile
 def clickedOn(known,x,y,flag):
+    #uncovers a tile
     if flag==0:
         if mines[y][x]==1:
             known[y][x]=1
         elif mines[y][x]==0 and known[y][x]==0:
             known[y][x]=1
             known=uncoverZeroes(known)
+    #places a flag
     else:
         if known[y][x]==0:
             known[y][x]=2
         elif known[y][x]==2:
             known[y][x]=0
     return known
+#checks if you have reached the win or lose state
 def checkWin():
     l=0
     for i in range(0,dimy):
         for j in range(0,dimx):
-            if (known[i][j]==1 and not  mines[i][j]==1) or (mines[i][j]==1 and not known[i][j]==1):
+            if bool(mines[i][j]==1) != bool(known[i][j]==1):
                 l+=1
             if known[i][j]==1 and mines[i][j]==1:
                 return "Over"
@@ -122,6 +131,7 @@ def checkWin():
         return False
 mines,known=setUpMines(mines,known)
 prtscr()
+#the main loop
 while True:
     print("=============")
     fx="";fy="";flag=""
